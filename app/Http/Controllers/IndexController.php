83b04a8e70d8408e7a806d91ad2bb0c203a700af
mss <1987886462@qq.com>
2020-11-06 16:04:18 +0800

    322

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -25,6 +25,36 @@ class IndexController extends Controller
         }
     }
 
+    /**
+         * 处理推送事件
+         */
+        public function wxEvent()
+        {
+            $signature = $_GET["signature"];
+            $timestamp = $_GET["timestamp"];
+            $nonce = $_GET["nonce"];
+
+            $token = env('WX_TOKEN');
+            $tmpArr = array($token, $timestamp, $nonce);
+            sort($tmpArr, SORT_STRING);
+            $tmpStr = implode( $tmpArr );
+            $tmpStr = sha1( $tmpStr );
+
+            if( $tmpStr == $signature ){            //验证通过
+
+                // 1 接收数据
+                $xml_str = file_get_contents("php://input");
+
+                // 记录日志
+                file_put_contents('wx_event.log',$xml_str);
+                echo "";
+                die;
+
+            }else{
+                echo "";
+            }
+        }
+
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Route::get('/test','TestController@test1');
-Route::any('/wx','IndexController@index');  //微信接入
+Route::any('/wx','IndexController@wxEvent');  //微信接入 接受时间推送
 Route::get('/wx/token','IndexController@getAccessToken');        //获取access_token
