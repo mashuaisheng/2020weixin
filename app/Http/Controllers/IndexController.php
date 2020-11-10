aedee78686c02084443dba053167d443b85d64f8
mss <1987886462@qq.com>
2020-11-10 10:31:21 +0800

    回复

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,76 +8,18 @@ use GuzzleHttp\Client;
 class IndexController extends Controller
 {
     //接入微信
-    public function index()
-    	{
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
+    public function index(){
+                $signature = $_GET["signature"];
+                $timestamp = $_GET["timestamp"];
+                $nonce = $_GET["nonce"];
 
-        $token = env('WX_TOKEN');
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
+                $token = env('WX_TOKEN');
+                $tmpArr = array($token, $timestamp, $nonce);
+                sort($tmpArr, SORT_STRING);
+                $tmpStr = implode( $tmpArr );
+                $tmpStr = sha1( $tmpStr );
 
-        if( $tmpStr == $signature ){
-            $xml_str = file_get_contents('php://input');
-            $data = simplexml_load_string($xml_str, 'SimpleXMLElement', LIBXML_NOCDATA);
-                if (strtolower($data->MsgType) == "event") {
-                    //关注
-                    if (strtolower($data->Event == 'subscribe')) {
-                        //回复用户消息(纯文本格式)
-                        $toUser = $data->FromUserName;
-                        $fromUser = $data->ToUserName;
-                        $msgType = 'text';
-                        $content = '欢迎关注';
-                        //根据OPENID获取用户信息（并且入库）
-                            //1.获取openid
-                        $token=$this->token();
-                        $url="https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$token."&openid=".$toUser."&lang=zh_CN";
-                        file_put_contents('wx_event.log',$url);
-                        $user=file_get_contents($url);
-                        $user=json_decode($user,true);
-                        $wxuser=UserModel::where('openid',$user['openid'])->first();
-                        if(!empty($wxuser)){
-                            $content="欢迎回来";
-                        }else{
-                            $data=[
-                                        "subscribe" => $user['subscribe'],
-                                        "openid" => $user["openid"],
-                                        "nickname" => $user["nickname"],
-                                        "sex" => $user["sex"],
-                                        "city" => $user["city"],
-                                        "country" => $user["country"],
-                                        "province" => $user["province"],
-                                        "language" => $user["language"],
-                                        "headimgurl" => $user["headimgurl"],
-                                        "subscribe_time" => $user["subscribe_time"],
-                                        "subscribe_scene" => $user["subscribe_scene"]
-                            ];
-                            $data=UserModel::insert($data);
-                        }
-}
-                        //%s代表字符串(发送信息)
-                        $template = "<xml>
-                                <ToUserName><![CDATA[%s]]></ToUserName>
-                                <FromUserName><![CDATA[%s]]></FromUserName>
-                                <CreateTime>%s</CreateTime>
-                                <MsgType><![CDATA[%s]]></MsgType>
-                                <Content><![CDATA[%s]]></Content>
-                                </xml>";
-                        $info = sprintf($template, $toUser, $fromUser, time(), $msgType, $content);
-                        return $info;
-                    }
-                    //取关
-                    if (strtolower($data->Event == 'unsubscribe')) {
-                        //清除用户的信息
-                    }
-                }
-            }
     }
-
-
 
 
     /**
@@ -98,10 +40,20 @@ class IndexController extends Controller
             if( $tmpStr == $signature ){            //验证通过
 
                 // 1 接收数据
-                $xml_str = file_get_contents("php://input");
+                $xml_str = file_get_contents("php://input") . "\n\n";
 
                 // 记录日志
-                file_put_contents('wx_event.log',$xml_str);
+                file_put_contents('wx_event.log',$xml_str,FILE_APPEND);
+                //将接受来的数据转化为对象
+                $obj=simplexml_load_string($xml);//将文件转换成对象
+                if($obj->MsgType=="event"){
+                    if($obj->Event=="subscribe"){
+                        $content="欢迎关注";
+                        echo $this->responseText($obj,$content);
+                    }
+                }
+
+
                 echo "";
                 die;
 
@@ -164,17 +116,17 @@ class IndexController extends Controller
     }
 
     //处理消息
-        private function responseText($xml,$content){
-            $fromUserName=$xml->ToUserName;
-            $toUserName=$xml->FromUserName;
+        private function responseText($obj,$content){
+            $FromUserName=$xml->ToUserName;
+            $ToUserName=$xml->FromUserName;
             $time=time();
             $msgType="text";
             $template="<xml>
-                       <ToUserName><![CDATA[%s]]></ToUserName>
-                       <FromUserName><![CDATA[%s]]></FromUserName>
-                       <CreateTime>%s</CreateTime>
-                       <MsgType><![CDATA[%s]]></MsgType>
-                       <Content><![CDATA[%s]]></Content>
+                       <ToUserName><![CDATA[".$ToUserName."]]></ToUserName>
+                       <FromUserName><![CDATA[".$FromUserName."]]></FromUserName>
+                       <CreateTime>time()</CreateTime>
+                       <MsgType><![CDATA[text]]></MsgType>
+                       <Content><![CDATA[".$content."]]></Content>
                        </xml>";//发送//来自//时间//类型//内容
             echo sprintf($template,$toUserName,$fromUserName,$time,$msgType,$content);
         }
